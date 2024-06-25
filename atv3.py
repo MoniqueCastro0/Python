141e64684b3a3c36a4dd9b848489f1d49da70dcf
Monique <moniquenickollydec2gmail.com>
2024-06-25 15:05:45 -0300

    atv3

---
+++ atv3.py
@@ -0,0 +1,4 @@
+cidade = "São Paulo"
+print(f"{cidade}")
+cidade = "Rio de Janeiro"
+print(f"{cidade}")
---
+++ atv4.py
@@ -0,0 +1,7 @@
+string = "Variavel tipo String"
+numeInteiro = 10
+nomeFloat = 3.9
+teste = True
+
+print(f"{type(string)} {type(numeInteiro)} {type(nomeFloat)} {type(teste)}") #type mostra qual é o tipo da variavel
+
