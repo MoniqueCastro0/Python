38bb9c0df47c1a9d8d9e359319e38723d07a46a3
Monique <moniquenickollydec2gmail.com>
2024-06-25 15:48:25 -0300

    atv1

--- atv1-variaveis.py
+++ atv1-variaveis.py
@@ -1,5 +1,5 @@
 nomeProprio = input("Digite seu nome:") #variavel nomePropio recebe o nome
 apelido = input("Digite seu apelido:") #var apel recebe o apelido digitado no imput
-edereco = input("Digite seu endereço:")
+edereco = input("Digite seu endereço:") #var edereco recebe o que foi digitado no imput
 CidGodPostal = input('Digite sua cidade e código postal:')
 print(f'{nomeProprio} {apelido} {edereco} {CidGodPostal}') #printa o valor que digitado nos imput
--- atv3-variaveis.py
+++ atv3-variaveis.py
@@ -1,4 +1,4 @@
-cidade = "São Paulo"
+cidade = "São Paulo" #var cidade recebe um valor string
 print(f"{cidade}")
 cidade = "Rio de Janeiro"
 print(f"{cidade}")
