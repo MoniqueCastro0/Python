49ec00145dac11e6dcb54be655e36a5df1ba9bfe
Monique <moniquenickollydec2gmail.com>
2024-06-25 14:44:11 -0300

    atv2

---
+++ atv2.py
@@ -0,0 +1,5 @@
+nome = input('Digite um nome:')
+ano = input('Digite um ano:')
+
+print(f'{nome} é uma valente cavaleira, nascida no ano de {ano}. Certa manhã, {nome} acordou com um barulho terrível: um dragão se aproximava da aldeia. Somente {nome} poderia salvar os moradores da aldeia.')
+
