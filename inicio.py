fd8837224f7c59bda606078a01a5948817d4ec0c
Monique <moniquenickollydec2gmail.com>
2024-06-25 13:43:30 -0300

    novo arquivo

---
+++ inicio.py
@@ -0,0 +1,17 @@
+nome = input("Digite seu nome:")
+print("Olá," + nome)
+
+# - nome_completo = nome """""" nome_sobrenome - concatenando variaveis 
+#print(f"o resultado é {resultado}, e a conta de dois valores é {resultado2}")
+#print("o resultado é resultado" + resultado)
+
+
+#input - entrada / campo de solicitação
+#nome da var = valor a ser recebido
+#comioqueise = momeSobrenome
+#var - o valor/ conteudo vareia
+#constante - fixo
+#Tipos primitivos python
+#int - num inteiros
+#float - num real
+#bool - true, false
---
+++ pratica.py
@@ -0,0 +1,14 @@
+nome = "Tim Tester"
+idade = 20
+skill = "python"
+level1 = "iniciante"
+skill2 = "java"
+level2 = "senior"
+skill3 = "c++"
+level3 = "pleno"
+baixo = 2000
+alto = 3000
+
+print(f"Meu nome é {nome}, tenho {idade} anos. \nMinhas habilidades são {skill}, {level1}. {skill2}, {level2}. {skill3}, {level3}. \nProcuro um emprego com um salário de {baixo} a {alto} euros por mês.")
+
+#print(f"o resultado é {resultado}, e a conta de dois valores é {resultado2}")
