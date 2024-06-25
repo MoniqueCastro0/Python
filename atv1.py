3fbff10bd22b6607633fbbf3de1c7def912b9711
Monique <moniquenickollydec2gmail.com>
2024-06-25 14:16:44 -0300

    atv1_comentarios

--- atv1.py
+++ atv1.py
@@ -1,5 +1,5 @@
-nomeProprio = input("Digite seu nome:")
-apelido = input("Digite seu apelido:")
+nomeProprio = input("Digite seu nome:") #variavel nomePropio recebe o nome
+apelido = input("Digite seu apelido:") #var apel recebe o apelido digitado no imput
 edereco = input("Digite seu endereço:")
 CidGodPostal = input('Digite sua cidade e código postal:')
-print(f'{nomeProprio} {apelido} {edereco} {CidGodPostal}')
+print(f'{nomeProprio} {apelido} {edereco} {CidGodPostal}') #printa o valor que digitado nos imput
