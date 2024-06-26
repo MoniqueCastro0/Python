609b79f5dffca260b30b5b28821875964558bf07
Monique <moniquenickollydec2gmail.com>
2024-06-26 14:33:03 -0300

    atv11_JurosSimples

--- atv10_operadores.py
+++ atv10_operadores.py
@@ -2,4 +2,4 @@ valor = float(input("Digite um valor em reais:"))
 
 dolar = valor / 5.45
 
-print(f"{round(dolar,2)}") #round mostra apos a virgula
+print(f"Esse valor em Dolares fica: {round(dolar,2)}") #round mostra apos a virgula
---
+++ atv11_operadores.py
@@ -0,0 +1,8 @@
+#Juros Simples
+
+capital = float(input("Digite o capital ínicial:"))
+taxa = float(input("Digite o valor da taxa de Juros anual: "))
+Tempo = float(input("Digite o tempo em anos: "))
+juros = capital * (taxa / 100) * Tempo # taxa / 100 - para jogar para numero em porcentagem
+
+print(f"O valor Total em Juros é de: {juros}")
