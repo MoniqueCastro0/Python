6011080976b46af8b446d21701087dc2db4bc128
Monique <moniquenickollydec2gmail.com>
2024-06-26 15:36:19 -0300

    atv13_operadores

---
+++ atv13_operadores.py
@@ -0,0 +1,9 @@
+#Perimetro
+largura = float(input("Digite a largura do Retângulo: "))
+altura = float(input("Digite a Altura do Retângulo: "))
+
+area = largura * altura
+perimetro = 2 * (largura + altura)
+
+print(f"A area desse Retângulo é de: {area}")
+print(f"O perímetro desse Retângulo é de: {perimetro}")
