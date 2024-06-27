41a5eb0e97e4c42b889b8e4dcd38d4445d8299e2
Monique <moniquenickollydec2gmail.com>
2024-06-27 15:09:32 -0300

    condicoes_py

---
+++ condicoes.py
@@ -0,0 +1,27 @@
+#if condicao:
+    #bloco de código a ser execultado se a condição for veradeira:
+    #if - plavra chave qeu inicia a estrura de cndição:
+
+#idade = 18
+#if idade >= 18:
+    #print("Voce é maior de idade.")
+
+#numero = int(input("Digite um número: "))
+
+#if numero < 0:
+    #print("Esse número é negativo.")
+
+#if numero > 0:
+    #print("Esse número é Positivo.")
+
+#if numero == 0:
+    #print("Esse número é 0")
+
+    #esse espçamento é o que determina que será execultado dentro do if
+
+#IF Prática
+
+numero = int(input("Digite um número inteiro: "))
+
+if numero == 1984:
+    print("Orwell")
---
+++ maiorNLista.py
@@ -0,0 +1,13 @@
+#Maior n de uma lista;
+
+#lista = [3, 7, 8, 6, 10, 2] #lista = []  - lista.append(3)
+#print(max(lista))
+
+#for i in lista: #percorre cada posição e mostra os números
+    #print(i)
+
+#quantidadeNumDigitados = int(input("Qunatos números você vai digitar? "))
+
+#for a in range.(quantidadeNumDigitados): #enquanta a não tiver o valor de quantiadeNumDigitados faça
+    #num = float(input(f"Digite o {a + 1}")) #a começa em 1, cada vez que o loop for repetir vai contar mais 1
+    #nums.append(num) #adiciona em uma lista os valores de num
