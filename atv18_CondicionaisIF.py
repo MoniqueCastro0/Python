de9acf0d90b5c1f50e8fffa89cd267993c2807a2
Monique <moniquenickollydec2gmail.com>
2024-06-27 17:19:14 -0300

    atv18_CondicionaisIF

---
+++ atv18_CondicionaisIF.py
@@ -0,0 +1,10 @@
+numero = int(input("Digite um número: "))
+
+if numero < 1000 and numero > 100 and numero > 10:
+    print("Esse número é menor que 1000 Obrigado!")
+
+if numero < 1000 and numero < 100 and numero > 10:
+    print("Esse número é menor que 1000 Este número é menor que 100 Obrigado!")
+
+if numero < 1000 and numero < 100 and numero < 10 and numero > 0:
+    print("Esse número é menor que 1000, Este número é menor que 100, Este número é menor que 10 Obrigado!")
