cc9b20621adc0c4c2f7834a3741323cb684d1577
Monique <moniquenickollydec2gmail.com>
2024-07-01 16:19:55 -0300

    atv34_estruCondicionalElif

---
+++ atv34_estruCondicionalElif.py
@@ -0,0 +1,7 @@
+nome = input("Digite o seu nome: ")
+
+if nome == "Huguinho" or nome == "Zezinho" or nome == "Luisinho":
+    print("Você é um dos sobrinhos do Pato Donald")
+
+if nome == "Chiquinho" or nome == "Francisquinho":
+    print("Você é um sobrinho do Mickey Mouse")
