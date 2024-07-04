65b033d490465053c7018dfc7bdc6237f79b4b1e
Monique <moniquenickollydec2gmail.com>
2024-07-04 15:57:56 -0300

    atv50_Loop_simles-while.cond

---
+++ atv50_Loop_simles-while.cond.py
@@ -0,0 +1,10 @@
+soma = 0
+
+while soma < 100: #Enquanto for verdadeiro vai execultar
+    numero = int(input("Digite um número: ")) #cada vez que execulta vai pedir um número
+    soma += numero #soma inicialmente esta como 0 mas cada vez que rodar vai somar mais o numero
+    #2° vez que rodar vai ser o soma mais o numero digitado
+    print(soma)
+
+print("Fim")
+     
